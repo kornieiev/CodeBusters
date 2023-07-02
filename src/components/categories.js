e73679c6c06a722775ad63686bb79151c8b09eac
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-07-02 18:57:25 +0300

    add active

--- src/components/categories.js
+++ src/components/categories.js
@@ -4,6 +4,7 @@ import {getCategories} from '../api/index'
 import {getBooksByCategory} from '../api/index'
 
 const displayCategories = () => {
+  let lastClickedButton = null;
   getCategories()
     .then(response => {
       const categories = response.data;
@@ -11,18 +12,56 @@ const displayCategories = () => {
       const categoryRow = document.createElement('div');
       categoryRow.classList.add('category-row');
 
+      // Створюємо кнопку "Усі категорії"
+      const allCategoriesButton = document.createElement('button');
+      allCategoriesButton.innerText = 'Усі категорії';
+      allCategoriesButton.addEventListener('click', () => {
+        if (lastClickedButton !== allCategoriesButton) {
+          displayBooksByCategory(null);
+          if (lastClickedButton) {
+            lastClickedButton.classList.remove('active');
+          }
+          allCategoriesButton.classList.add('active');
+          lastClickedButton = allCategoriesButton;
+        } else {
+          allCategoriesButton.classList.remove('active');
+          lastClickedButton = null;
+        }
+      });
+      categoryRow.appendChild(allCategoriesButton);
+
+      // Створюємо кнопки для кожної категорії
       categories.forEach(category => {
         const categoryButton = document.createElement('button');
         categoryButton.innerText = category.list_name;
-        categoryButton.addEventListener('click', () => displayBooksByCategory(category.list_name));
+        categoryButton.addEventListener('click', () => {
+          if (lastClickedButton !== categoryButton) {
+            displayBooksByCategory(category.list_name);
+            if (lastClickedButton) {
+              lastClickedButton.classList.remove('active');
+            }
+            categoryButton.classList.add('active');
+            lastClickedButton = categoryButton;
+            allCategoriesButton.classList.remove('active');
+          }
+        });
         categoryRow.appendChild(categoryButton);
-      })
+      });
+
       categoryList.appendChild(categoryRow);
+
+      if (!lastClickedButton) {
+        allCategoriesButton.classList.add('active');
+        lastClickedButton = allCategoriesButton;
+      }
     })
     .catch(error => {
       console.log(error);
     });
 };
+
+
+
 
 const displayBooksByCategory = (category) => {
   const booksContainer = document.getElementById('books-list');
