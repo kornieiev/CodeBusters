77230523a51bfc415de8d2d9e0cbdb938247b47d
YevhenKhariuk <777genya2009@gmail.com>
2023-07-02 16:14:19 +0300

    356736

--- src/components/shoplist.js
+++ src/components/shoplist.js
@@ -1,92 +1,50 @@
-// тут логіка JS для сторінки SHOPPING-LIST
 // Отримання даних з localStorage
 const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
 
 // Функція для створення карточки з даними
 function createShoppingCard(book) {
-  const cardContainer = document.querySelector('.shopping-list-cardlist');
+  const cardListContainer = document.querySelector('.shopping-list-cardlist');
 
-  // Створення елементів карточки
-  const card = document.createElement('li');
-  card.classList.add('shopping-list-card');
+  const cardHTML = `
+    <li class="shopping-list-card">
+      <img class="shopping-list-cardlogo" src="../images/shopping-list-cardlogo-test.png" alt="book-logo" />
+      <div class="shopping-list-fullinfo">
+        <div class="shopping-list-cardheader">
+          <div class="cardheader-titlecontainer">
+            <h3 class="shopping-list-cardtitle">${book.title}</h3>
+            <h4 class="shopping-list-cardcategory">${book.category}</h4>
+          </div>
+          <button class="deletecard">Х</button>
+        </div>
+        <p class="shopping-list-carddescription">${book.description}</p>
+        <div class="shopping-list-cardfooter">
+          <h4 class="shopping-list-cardauthor">${book.author}</h4>
+          <ul class="shopping-list-shops">
+            ${generateShopLinks(book.shops)}
+          </ul>
+        </div>
+      </div>
+    </li>
+  `;
 
-  const logo = document.createElement('img');
-  logo.classList.add('shopping-list-cardlogo');
-  logo.src = '../images/shopping-list-cardlogo-test.png';
-  logo.alt = 'book-logo';
+  cardListContainer.innerHTML += cardHTML;
+}
 
-  const fullInfo = document.createElement('div');
-  fullInfo.classList.add('shopping-list-fullinfo');
-
-  const cardHeader = document.createElement('div');
-  cardHeader.classList.add('shopping-list-cardheader');
-
-  const titleContainer = document.createElement('div');
-  titleContainer.classList.add('cardheader-titlecontainer');
-
-  const title = document.createElement('h3');
-  title.classList.add('shopping-list-cardtitle');
-  title.textContent = book.title;
-
-  const category = document.createElement('h4');
-  category.classList.add('shopping-list-cardcategory');
-  category.textContent = book.category;
-
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('deletecard');
-  deleteButton.textContent = 'Х';
-
-  const description = document.createElement('p');
-  description.classList.add('shopping-list-carddescription');
-  description.textContent = book.description;
-
-  const cardFooter = document.createElement('div');
-  cardFooter.classList.add('shopping-list-cardfooter');
-
-  const author = document.createElement('h4');
-  author.classList.add('shopping-list-cardauthor');
-  author.textContent = book.author;
-
-  const shops = document.createElement('ul');
-  shops.classList.add('shopping-list-shops');
-
-  // Додавання посилань на магазини
-  book.shops.forEach(shop => {
-    const shopItem = document.createElement('li');
-    shopItem.classList.add('shopping-list-shopsitem');
-
-    const shopLink = document.createElement('a');
-    shopLink.href = shop.url;
-    shopLink.classList.add('shopping-list-shopslink');
-
-    const shopIcon = document.createElement('svg');
-    shopIcon.classList.add('shopping-list-shopsicon');
-    shopIcon.width = '16';
-    shopIcon.height = '16';
-
-    const useElement = document.createElement('use');
-    useElement.href = '../images/sprite.svg#icon-burger';
-
-    shopIcon.appendChild(useElement);
-    shopLink.appendChild(shopIcon);
-    shopItem.appendChild(shopLink);
-    shops.appendChild(shopItem);
-  });
-
-  // Додавання елементів до карточки
-  titleContainer.appendChild(title);
-  titleContainer.appendChild(category);
-  cardHeader.appendChild(titleContainer);
-  cardHeader.appendChild(deleteButton);
-  fullInfo.appendChild(cardHeader);
-  fullInfo.appendChild(description);
-  cardFooter.appendChild(author);
-  cardFooter.appendChild(shops);
-  fullInfo.appendChild(cardFooter);
-  card.appendChild(logo);
-  card.appendChild(fullInfo);
-
-  cardContainer.appendChild(card);
+// Функція для генерації посилань на магазини
+function generateShopLinks(shops) {
+  return shops
+    .map(
+      shop => `
+    <li class="shopping-list-shopsitem">
+      <a href="${shop.url}" class="shopping-list-shopslink">
+        <svg class="shopping-list-shopsicon" width="16" height="16">
+          <use href="../images/sprite.svg#icon-burger"></use>
+        </svg>
+      </a>
+    </li>
+  `
+    )
+    .join('');
 }
 
 // Відображення карточок з даними з localStorage
