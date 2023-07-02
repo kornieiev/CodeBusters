ab0900336e8f4dc51b511c23789e5e4b5d9ba7ae
Ilydanyxx <danya.chuyko@icloud.com>
2023-07-02 13:02:00 +0300

    update/bookcard

--- src/components/bookcard.js
+++ src/components/bookcard.js
@@ -1,1 +1,17 @@
-import { fetchBooksByCategory } from "./categories";
+import { getBooksByCategory } from "../api";
+const a = document.querySelectorAll(".bestsellers-item-name")
+const bookTitle = document.querySelectorAll(".bookcard-name")
+const bookAuthor = document.querySelectorAll(".bookcard-author")
+const image = document.querySelectorAll(".bookcard-image")
+
+
+for (let i = 0; i < 4; i++) {
+    let category = a[i].textContent;
+    getBooksByCategory(category)
+        .then(response => {
+            bookTitle[i].textContent = response.data[0].title
+            bookAuthor[i].textContent = response.data[0].author
+            image[i].src = response.data[0].book_image
+        })
+    
+}
