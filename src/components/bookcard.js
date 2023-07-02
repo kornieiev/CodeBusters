88b576585397ce65da3ebdd00d8cfe5180a9a31a
Ilydanyxx <danya.chuyko@icloud.com>
2023-07-02 20:31:04 +0300

    update/bookcard

--- src/components/bookcard.js
+++ src/components/bookcard.js
@@ -2,17 +2,22 @@ import { getBooksByCategory } from "../api";
 
 export function getBookcard(categories, titles, authors, images) {
     
-
+console.log(titles)
     for (let i = 0; i < categories.length; i++) {
         let category = categories[i].textContent;
         getBooksByCategory(category)
             .then(response => {
-                const a = Math.floor(Math.random() * response.data.length);
-                titles[i].textContent = response.data[a].title
-                authors[i].textContent = response.data[a].author
-                images[i].src = response.data[a].book_image
+                
+                for (let j = 0; j < 20; j++) {
+                    const a = Math.floor(Math.random() * response.data.length);
+                    titles[j].textContent = response.data[a].title
+                    authors[j].textContent = response.data[a].author
+                    images[j].src = response.data[a].book_image
+                    
+                }
+            
             })
-    
+        
     }
     
 }
